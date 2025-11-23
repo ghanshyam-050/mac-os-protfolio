51b9753f699756680c3d3776622d433efdddf68e
Ghanshyam <ghanshyam991393@gmail.com>
2025-11-23 23:01:21 +0530

    navbar is complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "#components/Navbar"
+
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">My mac-Protfolio</h1>
-    </>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
